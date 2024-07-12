5da8462bc5e12cea00d81cd85a888ef4ac537785
BetaCat_HA <wangjianyu2311077@gmail.com>
2024-07-12 10:59:24 +0800

    优化通用卡片组件
    添加：通用卡片标题栏
    修复：通用卡片组件在微信小程序样式出错问题

--- src/components/common/card.vue
+++ src/components/common/card.vue
@@ -1,6 +1,25 @@
 <template>
   <view class="card">
-    <slot />
+    <!-- 如果有标题，则显示 -->
+    <view v-if="leftTitle || rightTitle || desc" class="card-header">
+      <view class="card-header-title">
+        <text class="card-header-title-text">
+          {{ leftTitle }}
+        </text>
+        <text class="card-header-title-text">
+          {{ rightTitle }}
+        </text>
+      </view>
+      <view v-if="desc" class="card-header-desc">
+        <text class="card-header-desc-text">
+          {{ desc }}
+        </text>
+      </view>
+    </view>
+
+    <view class="slot">
+      <slot />
+    </view>
   </view>
 </template>
 
@@ -8,6 +27,18 @@
 export default {
   name: 'Card',
   props: {
+    leftTitle: {
+      type: String,
+      default: '',
+    },
+    rightTitle: {
+      type: String,
+      default: '',
+    },
+    desc: {
+      type: String,
+      default: '',
+    },
   },
   data() {
     return {};
@@ -17,11 +48,39 @@ export default {
 
 <style lang="scss" scoped>
 .card {
-    width: 100%;
-    height: 100%;
-    background-color: #fff;
-    border-radius: 20rpx;
-    box-shadow: 0 0 20rpx rgba(0, 0, 0, 0.1);
+  width: 90vw;
+  height: 100%;
+  background-color: #fff;
+  border-radius: 20rpx;
+  box-shadow: 0 0 20rpx rgba(0, 0, 0, 0.1);
+  padding: 0 !important;
+
+  .card-header {
+    display: flex;
+    flex-direction: column;
+    background: #a7e6ff;
+    border-radius: 20rpx 20rpx 0 0;
+    .card-header-title {
+      padding: 10rpx 15rpx 10rpx 15rpx;
+      display: flex;
+      justify-content: space-between;
+    }
+    .card-header-desc {
+      padding: 10rpx 15rpx 10rpx 15rpx;
+      color: #999;
+    }
+  }
+
+  .slot {
+    // 就微信**搞特殊
+    /* #ifdef MP-WEIXIN */
     padding: 20rpx;
+    /* #endif */
+
+    /* #ifndef MP-WEIXIN */
+    margin: 20rpx;
+    /* #endif */
+
+  }
 }
 </style>
