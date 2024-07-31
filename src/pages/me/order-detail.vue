704965e37be27cde2dcc5b4fc84cc7b7cfe51309
zzsyy <13975325+zeng-shuying@user.noreply.gitee.com>
2024-07-31 09:24:41 +0800

    添加：订单详情页

---
+++ src/pages/me/order-detail.vue
@@ -0,0 +1,17 @@
+<template>
+  <view class="page-wrap">
+    <Card class="merchant-info-card" />
+
+    <Card>
+      <text>111</text>
+    </Card>
+
+    <Card>
+      <text>111</text>
+    </Card>
+
+    <Card>
+      <text>111</text>
+    </Card>
+  </view>
+</template>
