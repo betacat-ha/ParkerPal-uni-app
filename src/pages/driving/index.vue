ca33798154661e6ef35d5380aa69d21312b26804
Thehanged <thehanged@foxmail.com>
2024-08-20 13:36:52 +0800

    新增：地图支持自定义图层

--- src/pages/driving/index.vue
+++ src/pages/driving/index.vue
@@ -1,45 +1,24 @@
-<template>
-  <view>
-    <map
-      id="map" class="map"
-      :latitude="lat"
-      :longitude="lng"
-      :scale="scale"
-      :show-location="true"
-      :show-compass="true"
-      :enable-rotate="true"
-      :enable-overlooking="true"
-      :enable-3D="true"
-      :enable-indoorMap="true"
-      @markertap="handleMarkerClick"
-    />
-
-    <Tabbar />
-  </view>
-</template>
-
 <script setup lang="ts">
-import { useUserStore } from '@/store/index';
+import { useUserStore } from '@/store/index'
 
-// const apiKey = 'PYFBZ-Z4TLJ-JFXFI-DPTWK-FED6K-P7FPL';
-const imgUrl = '../../static/images/mark.png';
+const imgUrl = '../../static/images'
 
-const lat = ref(23.131);
-const lng = ref(113.372);
-const scale = ref(17);
+const lat = ref(23.131)
+const lng = ref(113.372)
+const scale = ref(17)
 
 onShow(() => {
   // 设置当前tab
-  useUserStore().tabValue = 1;
-});
+  useUserStore().tabValue = 1
+})
 
-const mapContext = uni.createMapContext('map', this);
-addMarkers();
-// addGroundOverlay();
-console.log(mapContext);
+const mapContext = uni.createMapContext('map', this)
+addMarkers()
+addGroundOverlay()
+console.log(mapContext)
 mapContext.on('markerClusterCreate', (e) => {
-  console.log('markerClusterCreate', e);
-});
+  console.log('markerClusterCreate', e)
+})
 
 function addMarkers() {
   const positions = [
@@ -59,9 +38,9 @@ function addMarkers() {
       latitude: 23.131,
       longitude: 113.373,
     },
-  ];
+  ]
 
-  const markers: any[] = [];
+  const markers: any[] = []
 
   positions.forEach((p, i) => {
     markers.push(
@@ -80,50 +59,78 @@ function addMarkers() {
           content: `停车场 ${i + 1}`,
         },
       }, p),
-    );
-  });
+    )
+  })
 
   mapContext.addMarkers({
     markers,
     clear: false,
-  });
+    fail: (err) => {
+      console.log(`添加地图标记失败：${err}`)
+    },
+  })
 }
 
 // TODO
-// function addGroundOverlay() {
-//   // 获取当前地图的范围
-//   let region = null;
-//   mapContext.getRegion({
-//     success: (res) => {
-//       region = res;
-//       console.log(res);
-//     },
-//   });
+function addGroundOverlay() {
+  // 获取当前地图的范围
+  mapContext.getRegion({
+    fail: (err) => {
+      console.log(`获取地图范围失败：${err}`)
+    },
+  })
 
-//   // 计算图层放置的坐标
-//   // gcj02ToWgs84()
+  // 计算图层放置的坐标
+  // gcj02ToWgs84()
 
-//   const layer = {
-//     id: 1,
-//     src: `${imgUrl}/layer1.jpg`,
-//     visible: true,
-//     zindex: 1, // 图层绘制顺序
-//     opacity: 0.6, // 图层透明度
-//     bounds: { // 图片覆盖的经纬度范围
-//       northeast: region.northeast, // 东北角经纬度
-//       southwest: region.southwest, // 西南角经纬度
-//     },
-//   };
-//   console.log(layer);
-//   mapContext.addGroundOverlay(layer);
-// }
+  // test
+  mapContext.addGroundOverlay({
+    id: 1, // 大坑，和文档不一样
+    src: `${imgUrl}/layer1.jpg`,
+    visible: true,
+    opacity: 1, // 图层透明度
+    bounds: { // 图片覆盖的经纬度范围
+      northeast: {
+        latitude: 23.1312,
+        longitude: 113.3712,
+      },
+      southwest: {
+        latitude: 23.1321,
+        longitude: 113.3719,
+      },
+    },
+    fail: (err) => {
+      console.log(`添加图层失败：${err}`)
+    },
+  })
+}
 
 // TODO 点击标记平滑移动地图 translateMarker
-function handleMarkerClick(e) {
+function handleMarkerClick(e: any) {
   // moveToLocation
-  console.log(e);
+  console.log(e)
 }
 </script>
+
+<template>
+  <view>
+    <map
+      id="map" class="map"
+      :latitude="lat"
+      :longitude="lng"
+      :scale="scale"
+      :show-location="true"
+      :show-compass="true"
+      :enable-rotate="true"
+      :enable-overlooking="true"
+      :enable-3D="true"
+      :enable-indoorMap="true"
+      @markertap="handleMarkerClick"
+    />
+
+    <Tabbar />
+  </view>
+</template>
 
 <style lang="scss">
 .map{
