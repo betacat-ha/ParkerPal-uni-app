d716dc97a6bc2d6f7eeac6e9c842e03dd269a961
zzsyy <13975325+zeng-shuying@user.noreply.gitee.com>
2024-08-26 00:14:08 +0800

    修改：订单存储器设计

--- src/store/modules/order/index.ts
+++ src/store/modules/order/index.ts
@@ -1,9 +1,9 @@
-import { defineStore } from 'pinia'
-import type { OrderInfo, OrderState } from './types'
-import { getAllList, getList } from '@/api/order'
-import type { pageParams } from '@/api/order/types'
+import { defineStore } from 'pinia';
+import type { OrderInfo, OrderState } from './types';
+import { getAllOrders, getOrdersByPage } from '@/api/order';
+import type { pageParams } from '@/api/order/types';
 
-const useOrderStore = defineStore('oder', {
+const useOrderStore = defineStore('order', {
   state: (): OrderState => {
     return {
       list: [] as OrderInfo[],
@@ -11,37 +11,37 @@ const useOrderStore = defineStore('oder', {
       page: 0,
       pageSize: 0,
       hasNext: false,
-    }
+    };
   },
   getters: {
   },
   actions: {
-    setOderState(partial: Partial<OrderState>) {
-      this.$patch(partial)
+    setOrderState(partial: Partial<OrderState>) {
+      this.$patch(partial);
     },
     async fetchInfo(pageParams: pageParams) {
-      console.log('开始获取信息')
-      const result = await getList(pageParams)
-      this.setOderState(result)
+      console.log('开始获取信息');
+      const result = await getOrdersByPage(pageParams);
+      this.setOrderState(result);
+      console.log(result)
     },
     async fetchAllInfo() {
-      console.log('开始获取全部信息')
-      const result = await getAllList()
-      this.setOderState(result)
+      console.log('开始获取全部信息');
+      const result = await getAllOrders();
+      this.setOrderState(result);
     },
     async fetchNextInfo() {
       if (!this.hasNext) {
-        console.log('数据已到底')
-        return
+        console.log('数据已到底');
+        return;
       }
 
-      console.log('开始获取下一页信息')
-      const result = await getList({ page: this.page + 1, limit: this.pageSize })
-      // 追加数据
-      this.hasNext = result.hasNext
-      this.list = this.list.concat(result.list)
+      console.log('开始获取下一页信息');
+      const result = await getOrdersByPage({ page: this.page + 1, limit: this.pageSize });
+      this.hasNext = result.hasNext;
+      this.list = this.list.concat(result.list);
     },
   },
-})
+});
 
-export default useOrderStore
+export default useOrderStore;
