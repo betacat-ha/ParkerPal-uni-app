24b16c6c612b8a1bb125d20377a2e3ce3aadc339
zzsyy <13975325+zeng-shuying@user.noreply.gitee.com>
2024-08-26 00:12:42 +0800

    修改：订单获取接口设计

--- src/api/order/index.ts
+++ src/api/order/index.ts
@@ -1,16 +1,21 @@
 /**
  * 订单相关接口
  */
-import type { pageParams, pageResult } from './types'
-import { get, post } from '@/utils/request'
-import type { OrderInfo, OrderState } from '@/store/modules/order/types'
+import type { pageParams, pageResult } from './types';
+import { get, post } from '@/utils/request';
+import type { OrderInfo, OrderState } from '@/store/modules/order/types';
 
-enum URL {
-  allList = '/order/all',
-  listByPage = '/order/page',
-  listById = '/order/@{id}',
+enum OrderURL {
+  allOrders = '/order/all',
+  ordersByPage = '/order/page',
+  orderById = '/order/@{id}',
 }
 
-export const getAllList = () => get<OrderState>({ url: URL.allList })
-export const getList = (data: pageParams) => post<pageResult<OrderInfo>>({ url: URL.listByPage, data })
-export const getById = (id: string) => get<OrderInfo>({ url: URL.listById.replace('@{id}', id) })
+// 获取所有订单
+export const getAllOrders = () => get<OrderState>({ url: OrderURL.allOrders });
+
+// 按分页获取订单列表
+export const getOrdersByPage = (data: pageParams) => post<pageResult<OrderInfo>>({ url: OrderURL.ordersByPage, data });
+
+// 根据ID获取订单详情
+export const getOrderById = (id: string, catchError = false) => get<OrderInfo>({ url: OrderURL.orderById.replace('@{id}', id), custom: { catch: catchError } });
